7b09a49585c69557483778929e1c7b30a8cb2995
AlyssaLozano <mrs.alyssa.lozano@gmail.com>
2026-03-19 20:21:00 -0500

    Delete site.js

--- site.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('form').forEach((form) => {
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const msg = form.querySelector('.form-message');
-    if (msg) {
-      msg.textContent = 'Thanks. This GitHub Pages version stores no data. Connect your form to Formspree, Basin, or Google Forms to receive submissions.';
-    }
-  });
-});
